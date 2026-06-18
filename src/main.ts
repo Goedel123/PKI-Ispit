536fdf90abc9a232661435a258b8fd1548e50b59
Goedel123 <lazar.obradovic.23@singimail.rs>
2026-06-18 18:10:53 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import
 import { App } from './app/app';
+
+//@ts-ignore
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+//@ts-ignore
+import '@fortawesome/fontawesome-free/css/all.css';
+
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));

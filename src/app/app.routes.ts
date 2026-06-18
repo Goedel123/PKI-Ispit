1933cc3849c77957fd5425012f00c158a008272d
Goedel123 <lazar.obradovic.23@singimail.rs>
2026-06-18 17:18:37 +0200

    pocetak

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { Home } from './home/home';
+
+export const routes: Routes = [
+    { path: '**', redirectTo: 'home'},
+    { path: 'home', component: Home },
+    { path: '**', title: 'Home', component: Home }
+];

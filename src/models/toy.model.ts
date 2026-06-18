781a13189cf304772f900bf4b4e927a431dfd0aa
Goedel123 <lazar.obradovic.23@singimail.rs>
2026-06-18 19:46:05 +0200

    toyModel

---
+++ src/models/toy.model.ts
@@ -0,0 +1,14 @@
+import { ReservationModel } from "./reservation.model";
+
+export interface ToyModel {
+    Ime: string,
+    Opis: string,
+    tip: string,
+    uzrast: string,
+    cijnaGrupa: string,
+    datumProizvodnje: string,
+    cena: Number,
+    status: string,
+    ocena: Number,
+
+}
